0692389a21d1da7b96d218b5b8ce1d0bbc6a32b8
subinfo <juanc.monsalvem@gmail.com>
2016-03-11 16:58:30 -0500

    Update app.js

--- bidirectional/app.js
+++ bidirectional/app.js
@@ -390,7 +390,7 @@ function update () {
           .style("opacity", 1).select(".value")
           .text(function () {
             var additionalInstructions = g.children.length ? "\n(Double click to expand)" : "";
-            return g.name + "\nNet flow: " + formatFlow(g.value) + additionalInstructions;
+            return g.name + "\nAfecta: " + formatFlow((g.value+g.netFlow)/2) + additionalInstructions;
           });
     }
   });

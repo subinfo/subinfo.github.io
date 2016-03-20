5bdd2dbb7223192420de4e991192cc4f4d225fee
subinfo <juanc.monsalvem@gmail.com>
2016-03-19 23:08:48 -0500

    Update bihisankey.js

--- bidirectional/bihisankey.js
+++ bidirectional/bihisankey.js
@@ -374,7 +374,7 @@ d3.biHiSankey = function () {
     function initializeNodeYPosition() {
       nodesByXPosition.forEach(function (nodes) {
         nodes.forEach(function (node, i) {
-          node.y = i;
+          node.y = 100;
           node.heightAllowance = node.value * yScaleFactor + linkSpacing * node.linkSpaceCount;
         });
       });

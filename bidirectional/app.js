44fdabe847307500f5f0661130e768f7554a9eeb
subinfo <juanc.monsalvem@gmail.com>
2016-03-19 23:32:24 -0500

    Update app.js

--- bidirectional/app.js
+++ bidirectional/app.js
@@ -146,10 +146,10 @@ function update () {
  // init();
 
   function dragmove(node) {
-    //node.x = Math.max(0, Math.min(WIDTH - node.width, d3.event.x));
-    //node.y = Math.max(0, Math.min(HEIGHT - node.height, d3.event.y));
-    node.y = node.number*100;
-    node.x = node.number*100;
+    node.x = Math.max(0, Math.min(WIDTH - node.width, d3.event.x));
+    node.y = Math.max(0, Math.min(HEIGHT - node.height, d3.event.y));
+    //node.y = node.number*100;
+    //node.x = node.number*100;
     d3.select(this).attr("transform", "translate(" + node.x + "," + node.y + ")");
     biHiSankey.relayout();
     svg.selectAll(".node").selectAll("rect").attr("height", function (d) { return d.height; });
